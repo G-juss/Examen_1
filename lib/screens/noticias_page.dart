bfba703b51927ba5fd34e5affc9e41bc4814b1e3
Génesis M <genesis.medina@unitec.edu>
2024-11-04 20:22:58 -0600

    página de noticias

---
+++ lib/screens/noticias_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NoticiasPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Noticias')),
+      body: Center(child: Text('Bienvenido a la sección de noticias')),
+    );
+  }
+}
